4d123de0bcb4c4f00b11e65bee67ea34a609dacc
Myles Amphlett <mylesamphlett@gmail.com>
2025-02-26 12:08:23 +0000

    create repo for json parser project

---
+++ parse.rb
@@ -0,0 +1,4 @@
+# 1 : validate a simple JSON object "{}"
+# 2 : validate an object with a string key and string value
+# 3 : validate an object with string, numeric, boolean and null values
+# 4 : validate an object with object and array values
